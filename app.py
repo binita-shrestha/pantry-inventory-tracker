d7e3549b3070d423ef80c3c8029b98c7fd2df232
Binita Shrestha <shresthabinita710@gmail.com>
2026-04-24 14:25:59 -0500

    implemented basic add item workflow with inventory display

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__, template_folder="app/templates", static_folder="app/static")
+inventory = []
 
 @app.route("/")
 def home():
     return render_template("index.html")
 @app.route("/inventory")
-def inventory():
-    return render_template("inventory.html")
+def inventory_page():
+    return render_template("inventory.html", items=inventory)
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
+    if request.method == "POST":
+        item_name = request.form["item_name"]
+        category = request.form["category"]
+        quantity = int(request.form["quantity"])
+        date_received = request.form["date_received"]
+        expiration_date = request.form["expiration_date"]
+        minimum_stock = int(request.form["minimum_stock"])
+
+        item = {
+        "item_name": item_name,
+        "category": category,
+        "quantity": quantity,
+        "date_received": date_received,
+        "expiration_date": expiration_date,
+        "minimum_stock": minimum_stock
+        }
+
+        inventory.append(item)
+
     return render_template("add.html")
 
 @app.route("/alerts")
