90365b6d70766c52baf6b8691b0f77c207571959
Binita Shrestha <shresthabinita710@gmail.com>
2026-04-25 16:52:15 -0500

    connected add item workflow to sqlite database

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 from flask import Flask, render_template, request
+import sqlite3
+
 app = Flask(__name__, template_folder="app/templates", static_folder="app/static")
-inventory = []
+def get_db_connection():
+    connection = sqlite3.connect("database/pantry.db")
+    connection.row_factory = sqlite3.Row
+    return connection
 
 @app.route("/")
 def home():
     return render_template("index.html")
 @app.route("/inventory")
 def inventory_page():
-    return render_template("inventory.html", items=inventory)
+    connection = get_db_connection()
+
+    items = connection.execute("""
+        SELECT * FROM inventory
+        ORDER BY expiration_date ASC
+    """).fetchall()
+
+    connection.close()
+    return render_template("inventory.html", items=items)
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
@@ -19,17 +32,14 @@ def add():
         expiration_date = request.form["expiration_date"]
         minimum_stock = int(request.form["minimum_stock"])
 
-        item = {
-        "item_name": item_name,
-        "category": category,
-        "quantity": quantity,
-        "date_received": date_received,
-        "expiration_date": expiration_date,
-        "minimum_stock": minimum_stock
-        }
-
-        inventory.append(item)
-
+        connection = get_db_connection()
+        connection.execute("""
+            INSERT INTO inventory 
+            (item_name, category, quantity, date_received, expiration_date, minimum_stock)
+            VALUES (?, ?, ?, ?, ?, ?)
+        """, (item_name, category, quantity, date_received, expiration_date, minimum_stock))
+        connection.commit()
+        connection.close()
     return render_template("add.html")
 
 @app.route("/alerts")
